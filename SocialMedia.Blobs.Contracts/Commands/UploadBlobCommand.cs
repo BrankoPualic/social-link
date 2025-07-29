117c7f9c80a89529e8c5c18ce5a2c5d162033d29
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-07-29 19:39:14 +0200

    feat - Blobs - Upload

---
+++ SocialMedia.Blobs.Contracts/Commands/UploadBlobCommand.cs
@@ -0,0 +1,7 @@
+﻿using SocialMedia.Blobs.Contracts.Dtos;
+using SocialMedia.SharedKernel;
+using SocialMedia.SharedKernel.UseCases;
+
+namespace SocialMedia.Blobs.Contracts.Commands;
+
+public sealed record UploadBlobCommand(FileInformationDto File, eBlobType BlobType) : Command<Guid>;
---
+++ SocialMedia.Blobs/Integrations/UploadBlobCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Result;
+using SocialMedia.Blobs.Application.Interfaces;
+using SocialMedia.Blobs.Contracts.Commands;
+using SocialMedia.SharedKernel.UseCases;
+
+namespace SocialMedia.Blobs.Integrations;
+
+internal class UploadBlobCommandHandler(IBlobService blobService) : MongoCommandHandler<UploadBlobCommand, Guid>
+{
+	public override async Task<Result<Guid>> Handle(UploadBlobCommand req, CancellationToken ct)
+	{
+		var file = req.File;
+		var blobType = req.BlobType;
+
+		var (blob, cleanup) = await blobService.UploadAsync(file, null, blobType);
+
+		await cleanup();
+
+		return Result.Success(blob.Id);
+	}
+}
