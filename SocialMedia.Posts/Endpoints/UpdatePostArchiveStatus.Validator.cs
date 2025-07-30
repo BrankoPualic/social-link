1f15755f88d6e5455ce41d5f9bb0093f33ad3f56
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-07-30 21:10:20 +0200

    feat - Posts - Update archive status

---
+++ SocialMedia.Posts/Endpoints/UpdatePostArchiveStatus.Validator.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using SocialMedia.Posts.Application.Dtos;
+using SocialMedia.Posts.Domain;
+using SocialMedia.SharedKernel;
+
+namespace SocialMedia.Posts.Endpoints;
+
+internal class UpdatePostArchiveStatusValidator : Validator<PostEditDto>
+{
+	public UpdatePostArchiveStatusValidator()
+	{
+		RuleFor(_ => _.Id)
+			.NotNull()
+			.NotEmpty()
+			.WithMessage(ResourcesValidation.Required(nameof(Post.Id)));
+	}
+}
