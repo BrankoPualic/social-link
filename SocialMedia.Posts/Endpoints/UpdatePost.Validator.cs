59ec48e0a9fa4d6fde060237018046e6e77f1c03
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-07-30 21:01:00 +0200

    feat - Posts - Update post

--- SocialMedia.Posts/Endpoints/UpdatePost.Validator.cs
+++ SocialMedia.Posts/Endpoints/UpdatePost.Validator.cs
@@ -1,20 +1,26 @@
 ﻿using FastEndpoints;
 using FluentValidation;
+using SocialMedia.Posts.Application.Dtos;
 using SocialMedia.Posts.Domain;
 using SocialMedia.SharedKernel;
 
 namespace SocialMedia.Posts.Endpoints;
 
-internal class CreatePostValidator : Validator<CreatePostRequest>
+internal class UpdatePostValidator : Validator<PostEditDto>
 {
-	public CreatePostValidator()
+	public UpdatePostValidator()
 	{
-		RuleFor(_ => _.Model.UserId)
+		RuleFor(_ => _.Id)
+			.NotNull()
+			.NotEmpty()
+			.WithMessage(ResourcesValidation.Required(nameof(Post.Id)));
+
+		RuleFor(_ => _.UserId)
 			.NotNull()
 			.NotEmpty()
 			.WithMessage(ResourcesValidation.Required(nameof(Post.UserId)));
 
-		RuleFor(_ => _.Model.Description)
+		RuleFor(_ => _.Description)
 			.MaximumLength(500)
 			.WithMessage(ResourcesValidation.MaximumLength(nameof(Post.Description), 500));
 	}
