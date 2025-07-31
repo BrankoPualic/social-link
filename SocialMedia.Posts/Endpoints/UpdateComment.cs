666e9f925b94d6a43abd8574d8e0e463c758d068
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-07-31 08:17:52 +0200

    feat - Posts - Update comment

---
+++ SocialMedia.Posts/Endpoints/UpdateComment.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.Result;
+using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using SocialMedia.Posts.Application.Dtos;
+using SocialMedia.Posts.Application.UseCases.Commands;
+
+namespace SocialMedia.Posts.Endpoints;
+
+[Authorize]
+internal class UpdateComment(IMediator mediator) : Endpoint<CommentEditDto>
+{
+	public override void Configure()
+	{
+		Post("/posts/comments/update");
+	}
+
+	public override async Task HandleAsync(CommentEditDto req, CancellationToken ct)
+	{
+		var result = await mediator.Send(new UpdateCommentCommand(req), ct);
+
+		if (result.IsNoContent())
+		{
+			await Send.NoContentAsync(ct);
+		}
+		else if (result.IsNotFound())
+		{
+			await Send.NotFoundAsync(ct);
+		}
+	}
+}
