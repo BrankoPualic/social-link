bdab2ee06ffb2e82aaca13fcc7667f7de7ba1ecb
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-09-13 19:39:40 +0200

    feat - Posts - Get user post query contract + Default avatar images

---
+++ SocialLink.Post.Contract/GetUserPostCountQuery.cs
@@ -0,0 +1,4 @@
+﻿using SocialLink.SharedKernel.UseCases;
+
+namespace SocialLink.Posts.Contracts;
+public sealed record GetUserPostCountQuery(Guid UserId) : Query<int>;
---
+++ SocialLink.Posts/Integrations/GetUserPostCountQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.Result;
+using Microsoft.EntityFrameworkCore;
+using SocialLink.Posts.Contracts;
+using SocialLink.SharedKernel.UseCases;
+
+namespace SocialLink.Posts.Integrations;
+
+internal class GetUserPostCountQueryHandler(IPostDatabaseContext db) : EFQueryHandler<GetUserPostCountQuery, int>(db)
+{
+	public override async Task<Result<int>> Handle(GetUserPostCountQuery req, CancellationToken ct)
+	{
+		var userId = req.UserId;
+		return await db.Posts
+			.Where(_ => _.UserId == userId)
+			.Where(_ => !_.IsArchived)
+			.CountAsync(ct);
+	}
+}
